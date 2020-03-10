e03863f1e9b6528e9b50c455afcf9bcbd996f212
NinjaERP <itsaraphap.t@gmail.com>
2020-03-10 10:20:08 +0700

    update

--- webhooks.php
+++ webhooks.php
@@ -9,43 +9,39 @@ $access_token = '82RJmA6R8Us7FXuVhejH4foQ9mEwGWdm07S4MUMB3Lf5mANVg01RzLnVIQbeLyG
 $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
+
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['source']['userId'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
+  foreach ($events['events'] as $event) {
+	if($event['type'] == "message" && isset($event['message']['text'])){
 
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
+	  $type = $event['source']['type']; // user , room , group
+	  $to = $event['source'][$type.'Id']; // userId , roomId , groupId
+	  $message = trim($event['message']['text']);
 
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+	  switch ($message) {
+		case '/help':
+		  $text = "ฉันคือ ID Finder Bot ยินดีที่ได้รู้จัก";
+		  $text .= "\nฉันมีหน้าที่ช่วยคุณค้าหา UserID RoomID หรือ GroupID ให้กับคุณ";
+		  $text .= "\nลองพิมพ์ /id ดูซิ";
+		  break;
+		case '/id':
+		  $text = "ข้อมูล ID ของคุณ";
+		  if(isset($event['source']['userId'])){ $text .= "\nUser ID : ".$event['source']['userId']; }
+		  if(isset($event['source']['roomId'])){ $text .= "\nRoom ID : ".$event['source']['roomId']; }
+		  if(isset($event['source']['groupId'])){ $text .= "\nGroup ID : ".$event['source']['groupId']; }
+		  break;
+		default:
+		  $text = NULL;
+		  break;
+	  }
 
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
+	  // message setup & send
+	  if($text != NULL){
+		$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text);
+		$response = $bot->replyMessage($to, $textMessageBuilder);
+	  }
 
-			echo $result . "\r\n";
-		}
 	}
+  }
 }
-echo "OK";
