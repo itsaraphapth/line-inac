31af47c5f884fc87e2bdb31692c4eba0c6c2d911
itsaraphapth <itsaraphap@icloud.com>
2020-07-02 17:58:27 +0700

    Update webhooks.2.php

--- webhooks.2.php
+++ webhooks.2.php
@@ -13,11 +13,7 @@ $events = json_decode($content, true);
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
   foreach ($events['events'] as $event) {
-	if($event['type'] == "message" && isset($event['message']['text'])){
-
-	  $type = $event['source']['type']; // user , room , group
-	  $to = $event['source'][$type.'Id']; // userId , roomId , groupId
-	  $message = trim($event['message']['text']);
+	if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 
 	  switch ($message) {
 		case '/help':
@@ -27,9 +23,36 @@ if (!is_null($events['events'])) {
 		  break;
 		case '/id':
 		  $text = "ข้อมูล ID ของคุณ";
-		  if(isset($event['source']['userId'])){ $text .= "\nUser ID : ".$event['source']['userId']; }
-		  if(isset($event['source']['roomId'])){ $text .= "\nRoom ID : ".$event['source']['roomId']; }
-		  if(isset($event['source']['groupId'])){ $text .= "\nGroup ID : ".$event['source']['groupId']; }
+		 // Get text sent
+			$text = $event['source']['userId'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
 		  break;
 		default:
 		  $text = NULL;
