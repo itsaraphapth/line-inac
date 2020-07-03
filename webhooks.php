b7f7d9cbb88b96beedbc791f679cb8399215b13c
itsaraphap <itsaraphap.t@gmail.com>
2020-07-03 10:27:11 +0700

    update

--- webhooks.php
+++ webhooks.php
@@ -13,58 +13,59 @@ $events = json_decode($content, true);
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
   foreach ($events['events'] as $event) {
-	if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+	// if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 
-	  switch ($message) {
-		case 'help':
-		  $text = "ฉันคือ ID Finder Bot ยินดีที่ได้รู้จัก";
-		  $text .= "\nฉันมีหน้าที่ช่วยคุณค้าหา UserID RoomID หรือ GroupID ให้กับคุณ";
-		  $text .= "\nลองพิมพ์ /id ดูซิ";
-		  break;
-		case 'id':
-		  $text = "ข้อมูล ID ของคุณ";
-		 // Get text sent
-			$text = $event['source']['userId'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
+	//   switch ($message) {
+	// 	case 'help':
+	// 	  $text = "ฉันคือ ID Finder Bot ยินดีที่ได้รู้จัก";
+	// 	  $text .= "\nฉันมีหน้าที่ช่วยคุณค้าหา UserID RoomID หรือ GroupID ให้กับคุณ";
+	// 	  $text .= "\nลองพิมพ์ /id ดูซิ";
+	// 	  break;
+	// 	case 'id':
+	// 	  $text = "ข้อมูล ID ของคุณ";
+	// 	 // Get text sent
+	// 		$text = $event['source']['userId'];
+	// 		// Get replyToken
+	// 		$replyToken = $event['replyToken'];
 
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
+	// 		// Build message to reply back
+	// 		$messages = [
+	// 			'type' => 'text',
+	// 			'text' => $text
+	// 		];
 
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+	// 		// Make a POST Request to Messaging API to reply to sender
+	// 		$url = 'https://api.line.me/v2/bot/message/reply';
+	// 		$data = [
+	// 			'replyToken' => $replyToken,
+	// 			'messages' => [$messages],
+	// 		];
+	// 		$post = json_encode($data);
+	// 		$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
+	// 		$ch = curl_init($url);
+	// 		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	// 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	// 		curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+	// 		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	// 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	// 		$result = curl_exec($ch);
+	// 		curl_close($ch);
 
-			echo $result . "\r\n";
-		  break;
-		default:
-		  $text = NULL;
-		  break;
-	  }
+	// 		echo $result . "\r\n";
+	// 	  break;
+	// 	default:
+	// 	  $text = NULL;
+	// 	  break;
+	//   }
 
-	  // message setup & send
-	  if($text != NULL){
-		$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event['type']);
+	 
+
+	// }
+	 // message setup & send
+	 if($text != NULL){
+		$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("OMG");
 		$response = $bot->replyMessage($to, $textMessageBuilder);
 	  }
-
-	}
   }
 }
