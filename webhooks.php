b3fe1651e70fd4600ab91324aa3175abad40699e
itsaraphap <itsaraphap.t@gmail.com>
2020-07-03 21:35:16 +0700

    tt

--- webhooks.php
+++ webhooks.php
@@ -1,105 +1,176 @@
 <?php
     $accessToken = "82RJmA6R8Us7FXuVhejH4foQ9mEwGWdm07S4MUMB3Lf5mANVg01RzLnVIQbeLyGly6AF1Zm1LjUo1XAk1I4fJKT+qRgUwONfQtwN+KxohE+prow+FC7B/JDmctO55d2I7pn2o0lgcLidPVgZr8g0vgdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
     
-    $content = file_get_contents('php://input');
-    $arrayJson = json_decode($content, true);
+    $request = file_get_contents('php://input');
+    $request_array  = json_decode($content, true);
     
     $arrayHeader = array();
     $arrayHeader[] = "Content-Type: application/json";
     $arrayHeader[] = "Authorization: Bearer {$accessToken}";
     
-    //รับข้อความจากผู้ใช้
-    $message = $arrayJson['events'][0]['message']['text'];
-#ตัวอย่าง Message Type "Text"
-    if($message == "สวัสดี"){
-        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-        $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
-        replyMsg($arrayHeader,$arrayPostData);
-    }
-    #ตัวอย่าง Message Type "Sticker"
-    else if($message == "ฝันดี"){
-        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-        $arrayPostData['messages'][0]['type'] = "sticker";
-        $arrayPostData['messages'][0]['packageId'] = "2";
-        $arrayPostData['messages'][0]['stickerId'] = "46";
-        replyMsg($arrayHeader,$arrayPostData);
-    }
-    #ตัวอย่าง Message Type "Image"
-    else if($message == "แมว"){
-        $image_url = "https://i.pinimg.com/originals/cc/22/d1/cc22d10d9096e70fe3dbe3be2630182b.jpg";
-        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-        $arrayPostData['messages'][0]['type'] = "image";
-        $arrayPostData['messages'][0]['originalContentUrl'] = $image_url;
-        $arrayPostData['messages'][0]['previewImageUrl'] = $image_url;
-        replyMsg($arrayHeader,$arrayPostData);
-    }
-    #ตัวอย่าง Message Type "Location"
-    else if($message == "พิกัดสยามพารากอน"){
-        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-        $arrayPostData['messages'][0]['type'] = "location";
-        $arrayPostData['messages'][0]['title'] = "สยามพารากอน";
-        $arrayPostData['messages'][0]['address'] =   "13.7465354,100.532752";
-        $arrayPostData['messages'][0]['latitude'] = "13.7465354";
-        $arrayPostData['messages'][0]['longitude'] = "100.532752";
-        replyMsg($arrayHeader,$arrayPostData);
-    }
-    #ตัวอย่าง Message Type "Text + Sticker ใน 1 ครั้ง"
-    else if($message == "ลาก่อน"){
-        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-        $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = "อย่าทิ้งกันไป";
-        $arrayPostData['messages'][1]['type'] = "sticker";
-        $arrayPostData['messages'][1]['packageId'] = "1";
-        $arrayPostData['messages'][1]['stickerId'] = "131";
-        replyMsg($arrayHeader,$arrayPostData);
-    }else if($message == "มีอะไรอีก"){
-		$arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-        $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = "อย่าทิ้งกันไป";
-        $arrayPostData['messages'][1]['quickReply'] = { // ②
-			"items": [
-			  {
-				"type": "action", // ③
-				"imageUrl": "https://example.com/sushi.png",
-				"action": {
-				  "type": "message",
-				  "label": "Sushi",
-				  "text": "Sushi"
-				}
-			  },
-			  {
-				"type": "action",
-				"imageUrl": "https://example.com/tempura.png",
-				"action": {
-				  "type": "message",
-				  "label": "Tempura",
-				  "text": "Tempura"
-				}
-			  },
-			  {
-				"type": "action", // ④
-				"action": {
-				  "type": "location",
-				  "label": "Send location"
-				}
-			  }
+	$request_array = json_decode($request, true);   // Decode JSON to Array
+
+	$jsonFlex = [
+		"type" => "flex",
+		"altText" => "Hello Flex Message",
+		"contents" => [
+		  "type" => "bubble",
+		  "direction" => "ltr",
+		  "header" => [
+			"type" => "box",
+			"layout" => "vertical",
+			"contents" => [
+			  [
+				"type" => "text",
+				"text" => "Purchase",
+				"size" => "lg",
+				"align" => "start",
+				"weight" => "bold",
+				"color" => "#009813"
+			  ],
+			  [
+				"type" => "text",
+				"text" => "฿ 100.00",
+				"size" => "3xl",
+				"weight" => "bold",
+				"color" => "#000000"
+			  ],
+			  [
+				"type" => "text",
+				"text" => "Rabbit Line Pay",
+				"size" => "lg",
+				"weight" => "bold",
+				"color" => "#000000"
+			  ],
+			  [
+				"type" => "text",
+				"text" => "2019.02.14 21:47 (GMT+0700)",
+				"size" => "xs",
+				"color" => "#B2B2B2"
+			  ],
+			  [
+				"type" => "text",
+				"text" => "Payment complete.",
+				"margin" => "lg",
+				"size" => "lg",
+				"color" => "#000000"
+			  ]
 			]
-		  };
-        replyMsg($arrayHeader,$arrayPostData);
+		  ],
+		  "body" => [
+			"type" => "box",
+			"layout" => "vertical",
+			"contents" => [
+			  [
+				"type" => "separator",
+				"color" => "#C3C3C3"
+			  ],
+			  [
+				"type" => "box",
+				"layout" => "baseline",
+				"margin" => "lg",
+				"contents" => [
+				  [
+					"type" => "text",
+					"text" => "Merchant",
+					"align" => "start",
+					"color" => "#C3C3C3"
+				  ],
+				  [
+					"type" => "text",
+					"text" => "BTS 01",
+					"align" => "end",
+					"color" => "#000000"
+				  ]
+				]
+			  ],
+			  [
+				"type" => "box",
+				"layout" => "baseline",
+				"margin" => "lg",
+				"contents" => [
+				  [
+					"type" => "text",
+					"text" => "New balance",
+					"color" => "#C3C3C3"
+				  ],
+				  [
+					"type" => "text",
+					"text" => "฿ 45.57",
+					"align" => "end"
+				  ]
+				]
+			  ],
+			  [
+				"type" => "separator",
+				"margin" => "lg",
+				"color" => "#C3C3C3"
+			  ]
+			]
+		  ],
+		  "footer" => [
+			"type" => "box",
+			"layout" => "horizontal",
+			"contents" => [
+			  [
+				"type" => "text",
+				"text" => "View Details",
+				"size" => "lg",
+				"align" => "start",
+				"color" => "#0084B6",
+				"action" => [
+				  "type" => "uri",
+				  "label" => "View Details",
+				  "uri" => "https://google.co.th/"
+				]
+			  ]
+			]
+		  ]
+		]
+	  ];
+	
+	
+	
+	if ( sizeof($request_array['events']) > 0 ) {
+		foreach ($request_array['events'] as $event) {
+			error_log(json_encode($event));
+			$reply_message = '';
+			$reply_token = $event['replyToken'];
+	
+	
+			$data = [
+				'replyToken' => $reply_token,
+				'messages' => [$jsonFlex]
+			];
+	
+			print_r($data);
+	
+			$post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+	
+			$send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
+	
+			echo "Result: ".$send_result."\r\n";
+			
+		}
 	}
-function replyMsg($arrayHeader,$arrayPostData){
-        $strUrl = "https://api.line.me/v2/bot/message/reply";
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL,$strUrl);
-        curl_setopt($ch, CURLOPT_HEADER, false);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
-        curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        $result = curl_exec($ch);
-        curl_close ($ch);
-    }
-   exit;
-?>
+	
+	echo "OK";
+	
+	
+	
+	
+	function send_reply_message($url, $post_header, $post_body)
+	{
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		$result = curl_exec($ch);
+		curl_close($ch);
+	
+		return $result;
+	}
+	
+	?>
