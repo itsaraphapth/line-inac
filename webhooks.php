54fb861a11c23494024b93dab21321611caee310
itsaraphap <itsaraphap.t@gmail.com>
2020-07-03 13:44:37 +0700

    u

--- webhooks.php
+++ webhooks.php
@@ -23,7 +23,10 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $text,
+				'type' => "sticker",
+				'packageId' => "2",
+				'stickerId' => "34";
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
