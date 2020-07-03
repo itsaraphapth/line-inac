fc682250b7bc038ad58de8df5242d0a0e25ad5da
itsaraphap <itsaraphap.t@gmail.com>
2020-07-03 10:59:07 +0700

    update

--- webhooks.php
+++ webhooks.php
@@ -16,7 +16,18 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['source']['userId'];
+			// $text = $event['source']['userId'];
+
+			if(isset($arrayJson['events']['userId']){
+				$text = $arrayJson['events']['userId'];
+			 }
+			 else if(isset($arrayJson['events']['groupId'])){
+				$text = $arrayJson['events']['groupId'];
+			 }
+			 else if(isset($arrayJson['events']['room'])){
+				$text = $arrayJson['events']['room'];
+			 }
+
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
