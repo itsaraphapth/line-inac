192b49a156b53a4e525f113e75ee9cce2b13df25
itsaraphap <itsaraphap.t@gmail.com>
2020-07-03 21:59:02 +0700

    ddd

--- webhooks.php
+++ webhooks.php
@@ -10,129 +10,167 @@ $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' 
 
 $request = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
+$message = $arrayJson['events'][0]['message']['text'];
+if($message == "สวัสดี"){
+	$json = [
+		"type" => "flex",
+		"altText" => "Hello Flex Message",
+		"contents" => [
+			"type" => "bubble",
+			"direction" => "ltr",
+			"header" => [
+				"type" => "box",
+				"layout" => "vertical",
+				"contents" => [
+				[
+					"type" => "text",
+					"text" => "Purchase",
+					"size" => "lg",
+					"align" => "start",
+					"weight" => "bold",
+					"color" => "#009813"
+				],
+				[
+					"type" => "text",
+					"text" => "฿ 100.00",
+					"size" => "3xl",
+					"weight" => "bold",
+					"color" => "#000000"
+				],
+				[
+					"type" => "text",
+					"text" => "Rabbit Line Pay",
+					"size" => "lg",
+					"weight" => "bold",
+					"color" => "#000000"
+				],
+				[
+					"type" => "text",
+					"text" => "2019.02.14 21:47 (GMT+0700)",
+					"size" => "xs",
+					"color" => "#B2B2B2"
+				],
+				[
+					"type" => "text",
+					"text" => "Payment complete.",
+					"margin" => "lg",
+					"size" => "lg",
+					"color" => "#000000"
+				]
+				]
+			],
+			"body" => [
+				"type" => "box",
+				"layout" => "vertical",
+				"contents" => [
+				[
+					"type" => "separator",
+					"color" => "#C3C3C3"
+				],
+				[
+					"type" => "box",
+					"layout" => "baseline",
+					"margin" => "lg",
+					"contents" => [
+					[
+						"type" => "text",
+						"text" => "Merchant",
+						"align" => "start",
+						"color" => "#C3C3C3"
+					],
+					[
+						"type" => "text",
+						"text" => "BTS 01",
+						"align" => "end",
+						"color" => "#000000"
+					]
+					]
+				],
+				[
+					"type" => "box",
+					"layout" => "baseline",
+					"margin" => "lg",
+					"contents" => [
+					[
+						"type" => "text",
+						"text" => "New balance",
+						"color" => "#C3C3C3"
+					],
+					[
+						"type" => "text",
+						"text" => "฿ 45.57",
+						"align" => "end"
+					]
+					]
+				],
+				[
+					"type" => "separator",
+					"margin" => "lg",
+					"color" => "#C3C3C3"
+				]
+				]
+			],
+			"footer" => [
+				"type" => "box",
+				"layout" => "horizontal",
+				"contents" => [
+				[
+					"type" => "text",
+					"text" => "View Details",
+					"size" => "lg",
+					"align" => "start",
+					"color" => "#0084B6",
+					"action" => [
+					"type" => "uri",
+					"label" => "View Details",
+					"uri" => "https://google.co.th/"
+					]
+				]
+				]
+			]
+		]
+	];
+else if($message == "ฝันดี"){
+	$json = [
+		"type"=> "text", // ①
+		"text"=> "Select your favorite food category or send me your location!",
+		"quickReply"=> [ // ②
+		  "items"=> [
+			[
+			  "type"=> "action", // ③
+			  "imageUrl"=> "https://example.com/sushi.png",
+			  "action"=> [
+				"type"=> "message",
+				"label"=> "Sushi",
+				"text"=> "Sushi"
+			  ]
+			],
+			[
+			  "type"=> "action",
+			  "imageUrl"=> "https://example.com/tempura.png",
+			  "action"=> [
+				"type"=> "message",
+				"label"=> "Tempura",
+				"text"=> "Tempura"
+			  ]
+			],
+			[
+			  "type"=> "action", // ④
+			  "action"=> [
+				"type"=> "location",
+				"label"=> "Send location"
+			  ]
+			]
+		  ]
+		]
+	  ]
+}
 
-$jsonFlex = [
-    "type" => "flex",
-    "altText" => "Hello Flex Message",
-    "contents" => [
-      "type" => "bubble",
-      "direction" => "ltr",
-      "header" => [
-        "type" => "box",
-        "layout" => "vertical",
-        "contents" => [
-          [
-            "type" => "text",
-            "text" => "Purchase",
-            "size" => "lg",
-            "align" => "start",
-            "weight" => "bold",
-            "color" => "#009813"
-          ],
-          [
-            "type" => "text",
-            "text" => "฿ 100.00",
-            "size" => "3xl",
-            "weight" => "bold",
-            "color" => "#000000"
-          ],
-          [
-            "type" => "text",
-            "text" => "Rabbit Line Pay",
-            "size" => "lg",
-            "weight" => "bold",
-            "color" => "#000000"
-          ],
-          [
-            "type" => "text",
-            "text" => "2019.02.14 21:47 (GMT+0700)",
-            "size" => "xs",
-            "color" => "#B2B2B2"
-          ],
-          [
-            "type" => "text",
-            "text" => "Payment complete.",
-            "margin" => "lg",
-            "size" => "lg",
-            "color" => "#000000"
-          ]
-        ]
-      ],
-      "body" => [
-        "type" => "box",
-        "layout" => "vertical",
-        "contents" => [
-          [
-            "type" => "separator",
-            "color" => "#C3C3C3"
-          ],
-          [
-            "type" => "box",
-            "layout" => "baseline",
-            "margin" => "lg",
-            "contents" => [
-              [
-                "type" => "text",
-                "text" => "Merchant",
-                "align" => "start",
-                "color" => "#C3C3C3"
-              ],
-              [
-                "type" => "text",
-                "text" => "BTS 01",
-                "align" => "end",
-                "color" => "#000000"
-              ]
-            ]
-          ],
-          [
-            "type" => "box",
-            "layout" => "baseline",
-            "margin" => "lg",
-            "contents" => [
-              [
-                "type" => "text",
-                "text" => "New balance",
-                "color" => "#C3C3C3"
-              ],
-              [
-                "type" => "text",
-                "text" => "฿ 45.57",
-                "align" => "end"
-              ]
-            ]
-          ],
-          [
-            "type" => "separator",
-            "margin" => "lg",
-            "color" => "#C3C3C3"
-          ]
-        ]
-      ],
-      "footer" => [
-        "type" => "box",
-        "layout" => "horizontal",
-        "contents" => [
-          [
-            "type" => "text",
-            "text" => "View Details",
-            "size" => "lg",
-            "align" => "start",
-            "color" => "#0084B6",
-            "action" => [
-              "type" => "uri",
-              "label" => "View Details",
-              "uri" => "https://google.co.th/"
-            ]
-          ]
-        ]
-      ]
-    ]
-  ];
 
 
 
-if ( sizeof($request_array['events']) > 0 ) {
+
+  if ( sizeof($request_array['events']) > 0 ) {
     foreach ($request_array['events'] as $event) {
         error_log(json_encode($event));
         $reply_message = '';
@@ -141,7 +179,7 @@ if ( sizeof($request_array['events']) > 0 ) {
 
         $data = [
             'replyToken' => $reply_token,
-            'messages' => [$jsonFlex]
+            'messages' => [$json]
         ];
 
         print_r($data);
