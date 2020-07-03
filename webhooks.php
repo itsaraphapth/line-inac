63a4471fc929f894403f9a0a55ee0b96f75bf942
itsaraphap <itsaraphap.t@gmail.com>
2020-07-03 13:38:34 +0700

    upd

--- webhooks.php
+++ webhooks.php
@@ -49,59 +49,6 @@ $ch = curl_init();
    }
 exit;
 
+?>
 
 
-
-
-// //Validate parsed JSON data
-// if (!is_null($events['events'])) {
-// 	// Loop through each event
-// 	foreach ($events['events'] as $event) {
-// 		// Reply only when message sent is in 'text' format
-// 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-// 			// Get text sent
-// 			$text = $event['source']['groupId'];
-
-// 			// if($event['source']['userId'){
-// 			// 	$text = $event['source']['userId'];
-// 			//  }
-// 			//  else if($event['source']['groupId']){
-// 			// 	$text = $event['source']['groupId'];
-// 			//  }
-// 			//  else if($event['source']['room']){
-// 			// 	$text = $event['source']['room'];
-// 			//  }
-
-// 			// Get replyToken
-// 			$replyToken = $event['replyToken'];
-
-// 			// Build message to reply back
-// 			$messages = [
-// 				'type' => 'text',
-// 				'text' => $text,
-
-// 			];
-
-// 			// Make a POST Request to Messaging API to reply to sender
-// 			$url = 'https://api.line.me/v2/bot/message/reply';
-// 			$data = [
-// 				'replyToken' => $replyToken,
-// 				'messages' => [$messages],
-// 			];
-// 			$post = json_encode($data);
-// 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-// 			$ch = curl_init($url);
-// 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-// 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-// 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-// 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-// 			$result = curl_exec($ch);
-// 			curl_close($ch);
-
-// 			echo $result . "\r\n";
-// 		}
-// 	}
-// }
-// echo "OK";
