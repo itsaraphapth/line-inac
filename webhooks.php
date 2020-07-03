0f210ca820426a904ae77a4a59dc55eec1c735f3
itsaraphap <itsaraphap.t@gmail.com>
2020-07-03 22:26:17 +0700

    ff

--- webhooks.php
+++ webhooks.php
@@ -11,127 +11,126 @@ $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' 
 $request = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
 $message = $request_array['events']['message']['text'];
-var_export($message);
-// if($message == "สวัสดี"){
-// 	$json = [
-// 		"type" => "flex",
-// 		"altText" => "Hello Flex Message",
-// 		"contents" => [
-// 			"type" => "bubble",
-// 			"direction" => "ltr",
-// 			"header" => [
-// 				"type" => "box",
-// 				"layout" => "vertical",
-// 				"contents" => [
-// 				[
-// 					"type" => "text",
-// 					"text" => "Purchase",
-// 					"size" => "lg",
-// 					"align" => "start",
-// 					"weight" => "bold",
-// 					"color" => "#009813"
-// 				],
-// 				[
-// 					"type" => "text",
-// 					"text" => "฿ 100.00",
-// 					"size" => "3xl",
-// 					"weight" => "bold",
-// 					"color" => "#000000"
-// 				],
-// 				[
-// 					"type" => "text",
-// 					"text" => "Rabbit Line Pay",
-// 					"size" => "lg",
-// 					"weight" => "bold",
-// 					"color" => "#000000"
-// 				],
-// 				[
-// 					"type" => "text",
-// 					"text" => "2019.02.14 21:47 (GMT+0700)",
-// 					"size" => "xs",
-// 					"color" => "#B2B2B2"
-// 				],
-// 				[
-// 					"type" => "text",
-// 					"text" => "Payment complete.",
-// 					"margin" => "lg",
-// 					"size" => "lg",
-// 					"color" => "#000000"
-// 				]
-// 				]
-// 			],
-// 			"body" => [
-// 				"type" => "box",
-// 				"layout" => "vertical",
-// 				"contents" => [
-// 				[
-// 					"type" => "separator",
-// 					"color" => "#C3C3C3"
-// 				],
-// 				[
-// 					"type" => "box",
-// 					"layout" => "baseline",
-// 					"margin" => "lg",
-// 					"contents" => [
-// 					[
-// 						"type" => "text",
-// 						"text" => "Merchant",
-// 						"align" => "start",
-// 						"color" => "#C3C3C3"
-// 					],
-// 					[
-// 						"type" => "text",
-// 						"text" => "BTS 01",
-// 						"align" => "end",
-// 						"color" => "#000000"
-// 					]
-// 					]
-// 				],
-// 				[
-// 					"type" => "box",
-// 					"layout" => "baseline",
-// 					"margin" => "lg",
-// 					"contents" => [
-// 					[
-// 						"type" => "text",
-// 						"text" => "New balance",
-// 						"color" => "#C3C3C3"
-// 					],
-// 					[
-// 						"type" => "text",
-// 						"text" => "฿ 45.57",
-// 						"align" => "end"
-// 					]
-// 					]
-// 				],
-// 				[
-// 					"type" => "separator",
-// 					"margin" => "lg",
-// 					"color" => "#C3C3C3"
-// 				]
-// 				]
-// 			],
-// 			"footer" => [
-// 				"type" => "box",
-// 				"layout" => "horizontal",
-// 				"contents" => [
-// 				[
-// 					"type" => "text",
-// 					"text" => "View Details",
-// 					"size" => "lg",
-// 					"align" => "start",
-// 					"color" => "#0084B6",
-// 					"action" => [
-// 					"type" => "uri",
-// 					"label" => "View Details",
-// 					"uri" => "https://google.co.th/"
-// 					]
-// 				]
-// 				]
-// 			]
-// 		]
-// 	];
-// else if($message == "ฝันดี"){
+if($message == "สวัสดี"){
+	$json = [
+		"type" => "flex",
+		"altText" => "Hello Flex Message",
+		"contents" => [
+			"type" => "bubble",
+			"direction" => "ltr",
+			"header" => [
+				"type" => "box",
+				"layout" => "vertical",
+				"contents" => [
+				[
+					"type" => "text",
+					"text" => "Purchase",
+					"size" => "lg",
+					"align" => "start",
+					"weight" => "bold",
+					"color" => "#009813"
+				],
+				[
+					"type" => "text",
+					"text" => "฿ 100.00",
+					"size" => "3xl",
+					"weight" => "bold",
+					"color" => "#000000"
+				],
+				[
+					"type" => "text",
+					"text" => "Rabbit Line Pay",
+					"size" => "lg",
+					"weight" => "bold",
+					"color" => "#000000"
+				],
+				[
+					"type" => "text",
+					"text" => "2019.02.14 21:47 (GMT+0700)",
+					"size" => "xs",
+					"color" => "#B2B2B2"
+				],
+				[
+					"type" => "text",
+					"text" => "Payment complete.",
+					"margin" => "lg",
+					"size" => "lg",
+					"color" => "#000000"
+				]
+				]
+			],
+			"body" => [
+				"type" => "box",
+				"layout" => "vertical",
+				"contents" => [
+				[
+					"type" => "separator",
+					"color" => "#C3C3C3"
+				],
+				[
+					"type" => "box",
+					"layout" => "baseline",
+					"margin" => "lg",
+					"contents" => [
+					[
+						"type" => "text",
+						"text" => "Merchant",
+						"align" => "start",
+						"color" => "#C3C3C3"
+					],
+					[
+						"type" => "text",
+						"text" => "BTS 01",
+						"align" => "end",
+						"color" => "#000000"
+					]
+					]
+				],
+				[
+					"type" => "box",
+					"layout" => "baseline",
+					"margin" => "lg",
+					"contents" => [
+					[
+						"type" => "text",
+						"text" => "New balance",
+						"color" => "#C3C3C3"
+					],
+					[
+						"type" => "text",
+						"text" => "฿ 45.57",
+						"align" => "end"
+					]
+					]
+				],
+				[
+					"type" => "separator",
+					"margin" => "lg",
+					"color" => "#C3C3C3"
+				]
+				]
+			],
+			"footer" => [
+				"type" => "box",
+				"layout" => "horizontal",
+				"contents" => [
+				[
+					"type" => "text",
+					"text" => "View Details",
+					"size" => "lg",
+					"align" => "start",
+					"color" => "#0084B6",
+					"action" => [
+					"type" => "uri",
+					"label" => "View Details",
+					"uri" => "https://google.co.th/"
+					]
+				]
+				]
+			]
+		]
+	];
+else if($message == "ฝันดี"){
 	$json = [
 		"type"=> "text", 
 		"text"=> "Select your favorite food category or send me your location!",
@@ -165,7 +164,13 @@ var_export($message);
 		  ]
 		]
 	];
-// }
+}else{
+	$json =[
+		"type": "image",
+		"originalContentUrl": "https://i.pinimg.com/originals/cc/22/d1/cc22d10d9096e70fe3dbe3be2630182b.jpg",
+		"previewImageUrl": "https://i.pinimg.com/originals/cc/22/d1/cc22d10d9096e70fe3dbe3be2630182b.jpg"
+	]
+}
 
 
 
@@ -180,9 +185,7 @@ var_export($message);
 
         $data = [
             'replyToken' => $reply_token,
-            'messages' => [
-				['type' => 'text', 
-				 'text' => json_encode($request_array)]
+            'messages' => [$json]
         ];
 
         print_r($data);
