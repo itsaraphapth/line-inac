0e146e7b5980dcf53cb9179b1885a4b5d217fc69
itsaraphap <itsaraphap.t@gmail.com>
2020-07-08 22:10:46 +0700

    555

--- botpush.php
+++ botpush.php
@@ -234,7 +234,7 @@ $arrayHeader[] = "Authorization: Bearer {$token}";
         'messages' => [$arrayPostData]
     ];
     $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
-    print_r($post_body);
+    var_dump(json_decode($post_body));
     die();
     $send_result =   pushMsgjson($arrayHeader,$post_body);
     echo "Result: ".$send_result."\r\n";
