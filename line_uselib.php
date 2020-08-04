bfa117fbcbdb340cb541c6be1a63f6b1daae750c
itsaraphap <itsaraphap.t@gmail.com>
2020-08-04 16:53:32 +0700

    dd

--- line_uselib.php
+++ line_uselib.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
 session_start();
 require_once("LineLoginLib.php");
@@ -160,3 +168,6 @@ if(isset($_POST['lineLogout'])){
     $LineLogin->redirect("line_uselib.php");
 }
 ?>
+</body>
+</html>
+
