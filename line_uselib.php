36891a2ecfd440d9f77e39a872aefb4f85f37ae5
itsaraphap <itsaraphap.t@gmail.com>
2020-08-12 23:04:04 +0700

    fff

--- line_uselib.php
+++ line_uselib.php
@@ -88,12 +88,17 @@ if(isset($_SESSION['ses_login_userData_val']) && $_SESSION['ses_login_userData_v
     //     echo "<option value=".$row["compcode"].">".$row["company_name"] ."</option>";
         
     } ?>
-<select name="comp_code" id="compID" class="form-control">
-                            <option value="">เลือกcomp</option>
-                            <?php while($result = mysqli_fetch_assoc($query)){ ?>
-                                <option value="<?=$result['compcode']?>"><?=$result['company_name']?></option>
-                            <?php } ?>
+     <form>
+    <select name="comp_code" id="compcode" class="form-control">
+        <option value="">เลือกcomp</option>
+        <?php while($result = mysqli_fetch_assoc($query)){ ?>
+            <option value="<?=$result['compcode']?>"><?=$result['company_name']?></option>
+        <?php } ?>
+    </select>
+    <select name="amphure_id" id="member" class="form-control">
+                            <option value="">เลือกอำเภอ</option>
                         </select>
+                        </form>
 <?php 
     // // echo "<tr>";
     // echo "<td>" .$row["compcode"] .  "</td> "; 
