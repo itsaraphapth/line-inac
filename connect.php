4def1b338df5ac672c6aa1fc4dfd3bfd616703d0
itsaraphap <itsaraphap.t@gmail.com>
2020-08-12 22:47:12 +0700

    fff

--- connect.php
+++ connect.php
@@ -7,3 +7,4 @@ $info = array(
 );
 $conn = mysqli_connect($info['host'], $info['user'], $info['password'], $info['dbname']) or die('Error connection database!');
 mysqli_set_charset($conn, 'utf8');
+?>
