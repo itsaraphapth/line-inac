41352a41a2cd3f6adea6f3ef6981776991b080c9
itsaraphap <itsaraphap.t@gmail.com>
2020-08-13 00:41:54 +0700

    dddd

--- sync.php
+++ sync.php
@@ -1,12 +1,11 @@
 
 <?php
 include('connect.php');
-echo $_POST['memberID'];
-echo "<br>";
-echo $_POST['userID'];
-// $sql = "select * from member WHERE compcode='".$_GET['comp_code']."'";
-// $query = mysqli_query($conn, $sql);
- 
+$memberID =  $_GET['memberID'];
+$userID =  $_GET['userID'];
+$sql = "update member set lineid = '".$_GET['userID']."' WHERE m_code='".$_GET['memberID']."'";
+$query = mysqli_query($conn, $sql);
+mysqli_close($conn);
 // $json = array();
 // while($result = mysqli_fetch_assoc($query)) {    
 //     array_push($json, $result);
